4282d623f8e6947c66c098da06bb1cdcffa3c7cd
EnergonNER <stal.energon.ner@gmail.com>
2025-08-14 22:05:23 +0300

    Loot Table

--- src/main/java/energon/nebulalib/config/Config.java
+++ src/main/java/energon/nebulalib/config/Config.java
@@ -563,7 +563,7 @@ public class Config {
             for (String parts : test.get()) {
                 if (parts.startsWith(ElementType.STRING_LIST.getPrefix())) {
                     String[] check = parts.split(Pattern.quote(list1));
-                    ConfigBase base = new StringListElement(check[0].substring(3), check[1].split(Pattern.quote(list2)));
+                    ConfigBase base = new StringListElement(check[0].substring(3), check.length == 2 ? check[1].split(Pattern.quote(list2)) : new String[0]);
                     elements.add(base);
                 } else if (parts.startsWith(ElementType.STRING.getPrefix())) {
                     String[] check = parts.substring(2).split(Pattern.quote(ElementType.EQUALS.getPrefix()));
